84df0c3313e1407892515c36c088d30cf111b720
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2023-05-14 21:02:03 +0700

    edit scan id

--- query/scan_id.php
+++ query/scan_id.php
@@ -1,6 +1,6 @@
 <?php
 include "koneksi.php";
-$getID = $_GET['id'];
+$getID = htmlspecialchars($_GET['id']);
 
 $delete = mysqli_query($db, "DELETE FROM `temp_rfid` WHERE `temp_rfid`.`jenis_scan` = 'absen';");
 $insert = mysqli_query($db, "INSERT INTO `temp_rfid` (`rfid`, `jenis_scan`) VALUES ('$getID', 'absen')
