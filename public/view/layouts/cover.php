1cb2a737e94a5ea685773932fe9ae57387485c66
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2023-05-16 17:25:22 +0700

    add

--- public/view/layouts/cover.php
+++ public/view/layouts/cover.php
@@ -32,16 +32,16 @@
     <link href="public/assets/css/responsive.css" rel="stylesheet" />
     <link rel="shortcut icon" href="public/assets/favicon.ico" />
     <style>
-        #mobile-view {
-            display: none;
-        }
+    #mobile-view {
+        display: none;
+    }
 
-        @media (max-width: 992px) {
-            #mobile-view {
-                display: flex;
-                justify-content: center;
-            }
+    @media (max-width: 992px) {
+        #mobile-view {
+            display: flex;
+            justify-content: center;
         }
+    }
     </style>
 </head>
 
@@ -94,13 +94,10 @@
                         <div class="custom_menu-btn ">
                             <button>
                                 <span class=" s-1">
-
                                 </span>
                                 <span class="s-2">
-
                                 </span>
                                 <span class="s-3">
-
                                 </span>
                             </button>
                         </div>
@@ -827,44 +824,44 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js">
     </script>
     <script type="text/javascript">
-        $(".owl-carousel").owlCarousel({
-            loop: true,
-            margin: 10,
-            nav: true,
-            navText: [],
-            autoplay: true,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                420: {
-                    items: 2
-                },
-                1000: {
-                    items: 5
-                }
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [],
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            420: {
+                items: 2
+            },
+            1000: {
+                items: 5
             }
+        }
 
-        });
+    });
     </script>
     <script>
-        var nav = $("#navbarSupportedContent");
-        var btn = $(".custom_menu-btn");
-        btn.click
-        btn.click(function (e) {
+    var nav = $("#navbarSupportedContent");
+    var btn = $(".custom_menu-btn");
+    btn.click
+    btn.click(function(e) {
 
-            e.preventDefault();
-            nav.toggleClass("lg_nav-toggle");
-            document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
-        });
+        e.preventDefault();
+        nav.toggleClass("lg_nav-toggle");
+        document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
+    });
     </script>
     <script>
-        $('.carousel').on('slid.bs.carousel', function () {
-            $(".indicator-2 li").removeClass("active");
-            indicators = $(".carousel-indicators li.active").data("slide-to");
-            a = $(".indicator-2").find("[data-slide-to='" + indicators + "']").addClass("active");
-        })
+    $('.carousel').on('slid.bs.carousel', function() {
+        $(".indicator-2 li").removeClass("active");
+        indicators = $(".carousel-indicators li.active").data("slide-to");
+        a = $(".indicator-2").find("[data-slide-to='" + indicators + "']").addClass("active");
+    })
     </script>
 
 </body>
