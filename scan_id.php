0cf8eb73e8472b4c49485e56f65cd789ff79a302
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2023-05-19 10:18:13 +0700

    scan masi eror

--- scan_id.php
+++ scan_id.php
@@ -25,13 +25,14 @@ if ($waktu > "07:00:00") {
 }
 
 // mencari id siswa
-$data_siswa = mysqli_query($db, "SELECT id_siswa FROM tb_siswa WHERE uuid='$getID' LIMIT 1");
+$data_siswa = mysqli_query($db, "SELECT * FROM `tb_siswa`WHERE uuid = '$getID'");
 $result = mysqli_fetch_assoc($data_siswa);
-if (mysqli_num_rows($data_siswa) != 1) {
+if (mysqli_num_rows($data_siswa) < 1) {
     echo "uuid tidak terdaftar";
     return;
     // jika uuid tidak ada program berhenti
 }
+
 
 $id_siswa = $result['id_siswa'];
 // menghapus temporary dari mesin sebelumnya
@@ -40,9 +41,33 @@ $delete = mysqli_query($db, "DELETE FROM `temp_rfid` WHERE `temp_rfid`.`kode_mes
 $insert = mysqli_query($db, "INSERT INTO `temp_rfid` (`id_temp`, `rfid`, `jenis_scan`, `kode_mesin`, `last_use`)
 VALUES (NULL, '$getID', '$getStat', '$getMesin', '$waktu')");
 
-$qry_absen = mysqli_query($db, "SELECT id_absen_siswa, tgl, masuk FROM tb_absen_siswa WHERE id_siswa = '$id_siswa' AND tgl = '$tgl'");
+$qry_absen = mysqli_query($db, "SELECT id_absen_siswa, tgl, masuk FROM tb_absen_siswa WHERE id_siswa = '$id_siswa' AND tgl = '$tgl' ORDER BY id_absen_siswa");
 $absen_terbaru = mysqli_fetch_assoc($qry_absen);
 $row_absen = mysqli_num_rows($qry_absen);
+
+var_dump($absen_terbaru);
+$tempo = strtotime($waktu) - strtotime($absen_terbaru['masuk']);
+if ($absen_terbaru['tgl'] == $tgl) {
+    // update masuk
+    if ($tempo <= 600) {
+        // 600 detik
+        $id_absen = $absen_terbaru['id_absen_siswa'];
+        $absen = mysqli_query($db, "UPDATE `tb_absen_siswa` SET `masuk` = '$waktu', `status_absen` = '$ket'
+    WHERE `tb_absen_siswa`.`id_absen_siswa` = '$id_absen' ");
+        echo "masuk";
+        return;
+    }
+    // update pulang
+    if ($tempo >= 600) {
+        $id_absen = $absen_terbaru['id_absen_siswa'];
+        $absen = mysqli_query($db, "UPDATE `tb_absen_siswa` SET `pulang` = '$waktu', `status_absen` ='Selamat Jalan', `keterangan_absen` = 'Pulang'
+    WHERE `tb_absen_siswa`.`id_absen_siswa` = '$id_absen' ");
+        echo "pulang";
+        return;
+    }
+}
+
+
 // jarak waktu masuk dan scan berikutnya
 if ($row_absen < 1) {
     // masuk data baru
@@ -50,26 +75,5 @@ if ($row_absen < 1) {
 (`id_absen_siswa`, `id_siswa`, `tgl`, `masuk`, `keterangan_absen`, `status_absen`, `ket_a`)
 VALUES (NULL, '$id_siswa', '2023-05-18', '$waktu', 'Masuk', '$ket', '$ket_a');");
     echo "buat baru";
-    return;
-}
-
-
-$tempo = strtotime($waktu) - strtotime($absen_terbaru['masuk']);
-// update masuk
-if ($absen_terbaru['tgl'] == $tgl && $tempo <= 600) {
-    // 600 detik
-    $id_absen = $absen_terbaru['id_absen_siswa'];
-    $absen = mysqli_query($db, "UPDATE `tb_absen_siswa` SET `masuk` = '$waktu', `status_absen` = '$ket'
-    WHERE `tb_absen_siswa`.`id_absen_siswa` = '$id_absen' ");
-    echo "masuk";
-    return;
-}
-
-// update pulang
-if ($absen_terbaru['tgl'] == $tgl && $tempo >= 600) {
-    $id_absen = $absen_terbaru['id_absen_siswa'];
-    $absen = mysqli_query($db, "UPDATE `tb_absen_siswa` SET `pulang` = '$waktu', `status_absen` ='Selamat Jalan', `keterangan_absen` = 'Pulang'
-    WHERE `tb_absen_siswa`.`id_absen_siswa` = '$id_absen' ");
-    echo "pulang";
     return;
 }
