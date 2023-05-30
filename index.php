4d1ab2af478f94a3bb326d886b644481f1fd2809
Adimana12 <133749649+Adimana12@users.noreply.github.com>
2023-05-30 14:55:22 +0700

    update

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 //session untuk login
 session_start();
 // session untuk logout
-session_destroy();
+// session_destroy();
 // contoh session
-// $_SESSION['level'] = "admin";
+$_SESSION['level'] = "guru";
 include "query/koneksi.php";
 if (empty($_SESSION['level'])) {
     if (isset($_REQUEST['hlm'])) {
@@ -60,37 +60,37 @@ if ($_SESSION['level'] == "guru") {
     }
 }
 
-if ($_SESSION['level'] == "admin") {
-    $title = "admin | Sistem Absensi";
-    if (isset($_REQUEST['hlm'])) {
-        $hlm = $_REQUEST['hlm'];
-        switch ($hlm) {
-            case 'admin':
-                include "./public/view/layouts/admin/admin_dashboard.php";
-                break;
-            case 'kelas':
-                include "./public/view/layouts/admin/data_kelas.php";
-                break;
-            case 'guru':
-                include "./public/view/layouts/admin/data_guru.php";
-                break;
-            case 'siswa':
-                include "./public/view/layouts/admin/data_siswa.php";
-                break;
-            case 'login':
-                include "./public/view/layouts/login.php";
-                break;
-            case 'cover':
-                include "./public/view/layouts/cover.php";
-                break;
-            case '404':
-                include "./public/view/eror/404.php";
-                break;
-            default:
-                include "./public/view/layouts/admin/admin_dashboard.php";
-                break;
-        }
-    } else {
-        include "./public/view/layouts/cover.php";
-    }
-}
+// if ($_SESSION['level'] == "admin") {
+//     $title = "admin | Sistem Absensi";
+//     if (isset($_REQUEST['hlm'])) {
+//         $hlm = $_REQUEST['hlm'];
+//         switch ($hlm) {
+//             case 'admin':
+//                 include "./public/view/layouts/admin/admin_dashboard.php";
+//                 break;
+//             case 'kelas':
+//                 include "./public/view/layouts/admin/data_kelas.php";
+//                 break;
+//             case 'guru':
+//                 include "./public/view/layouts/admin/data_guru.php";
+//                 break;
+//             case 'siswa':
+//                 include "./public/view/layouts/admin/data_siswa.php";
+//                 break;
+//             case 'login':
+//                 include "./public/view/layouts/login.php";
+//                 break;
+//             case 'cover':
+//                 include "./public/view/layouts/cover.php";
+//                 break;
+//             case '404':
+//                 include "./public/view/eror/404.php";
+//                 break;
+//             default:
+//                 include "./public/view/layouts/admin/admin_dashboard.php";
+//                 break;
+//         }
+//     } else {
+//         include "./public/view/layouts/cover.php";
+//     }
+// }
