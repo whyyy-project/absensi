985fed63e7a3fdb18cba868b20259c25f434638b
Wahyu Nur Cahyo <116796627+whyyy24@users.noreply.github.com>
2023-07-20 17:25:46 +0700

    edit edit

--- public/view/layouts/guru/top-content.php
+++ public/view/layouts/guru/top-content.php
@@ -1,22 +1,25 @@
   <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
+  <div class="col-xl-3 col-md-6 col-sm-6 col-6 mb-4">
       <div class="card border-left-primary shadow h-100 py-2">
-          <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                  <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                          Jumlah Siswa</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totalSiswa ?></div>
-                  </div>
-                  <div class="col-auto">
-                      <i class="fas fa-users fa-2x text-gray-300"></i>
+          <a href="?hlm=wali" class="text-decoration-none">
+              <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                              Jumlah Siswa</div>
+                          <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totalSiswa ?></div>
+                      </div>
+                      <div class="col-auto">
+                          <i class="fas fa-users fa-2x text-gray-300"></i>
+                      </div>
                   </div>
               </div>
-          </div>
+          </a>
       </div>
   </div>
+
   <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
+  <div class="col-xl-3 col-md-6 col-sm-6 col-6 mb-4">
       <div class="card border-left-success shadow h-100 py-2">
           <div class="card-body">
               <div class="row no-gutters align-items-center">
@@ -34,7 +37,7 @@
   </div>
 
   <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
+  <div class="col-xl-3 col-md-6 col-sm-6 col-6 mb-4">
       <div class="card border-left-info shadow h-100 py-2">
           <div class="card-body">
               <div class="row no-gutters align-items-center">
@@ -61,7 +64,7 @@
   </div>
 
   <!-- Pending Requests Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
+  <div class="col-xl-3 col-md-6 col-sm-6 col-6 mb-4">
       <div class="card border-left-warning shadow h-100 py-2">
           <div class="card-body">
               <div class="row no-gutters align-items-center">
@@ -71,7 +74,7 @@
                       <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $tidakHadir ?> Siswa</div>
                   </div>
                   <div class="col-auto">
-                      <i class="fas fa-comments fa-2x text-gray-300"></i>
+                      <i class="fas fa-exclamation rotate-n-15 fa-2x text-gray-300"></i>
                   </div>
               </div>
           </div>
--- public/view/layouts/header.php
+++ public/view/layouts/header.php
@@ -15,16 +15,15 @@
 
     <!-- Custom fonts for this template-->
     <link href="public/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="public/assets/css/sb-admin-2.min.css" rel="stylesheet">
     <link href="public/assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
     <!-- icon -->
-    <link rel="shortcut icon" href="public/assets/favicon.ico">
+    <link rel="shortcut icon" href="public/images/abror.ico" />
+
 
 </head>
 
