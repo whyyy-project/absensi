0d9247303ec0c938d38e6e3e310e39bf9ae862e9
Wahyu Nur Cahyo <116796627+whyyy24@users.noreply.github.com>
2023-07-22 22:22:31 +0700

    .

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # absensi
 
 # project absensi
+
+# gabut
--- public/view/layouts/admin/tambah_siswa.php
+++ public/view/layouts/admin/tambah_siswa.php
@@ -28,6 +28,7 @@ include "public/view/partials/admin/admin_modal.php";
             <a href="?hlm=siswa" class="text-decoration-none text-white <?= $class ?>">lihat daftar Siswa</a>
         </div>
     </div>
+    <?php include "query/admin/kelas.php" ?>
     <div class="row">
         <div class="col-12">
             <div class="card shadow mb-4">
@@ -38,18 +39,34 @@ include "public/view/partials/admin/admin_modal.php";
                     <form action="?hlm=tambah-siswa" method="post" class="user">
                         <div class="form-group d-flex">
                             <label for="nama-siswa" class="col-4 mt-2 text-center">Nama Siswa </label>
-                            <input type="text" name="nama-siswa" id="nama-siswa" class="form-control form-control-user col-6" placeholder="Nama Siswa">
+                            <input type="text" name="nama-siswa" id="nama-siswa" class="text-center form-control col-6" placeholder="Nama Siswa">
+                        </div>
+                        <div class="form-group d-flex">
+                            <label for="nis" class="col-4 mt-2 text-center">NIS</label>
+                            <input type="text" name="nis" id="nama-kelas" class="text-center form-control col-6" placeholder="NIS Siswa">
                         </div>
                         <div class="form-group d-flex">
                             <label for="angkatan" class="col-4 mt-2 text-center">Angkatan</label>
-                            <input type="text" name="angkatan" id="nama-kelas" class="form-control form-control-user col-6" placeholder="Angkatan Kelas">
+                            <input type="text" name="angkatan" id="nama-kelas" class="text-center form-control col-6" placeholder="Angkatan Kelas">
                         </div>
                         <div class="form-group d-flex">
-                            <label for="angkatan" class="col-4 mt-2 text-center">Angkatan</label>
-                            <input type="text" name="angkatan" id="nama-kelas" class="form-control form-control-user col-6" placeholder="Angkatan Kelas">
+                            <label for="idcard" class="col-4 mt-2 text-center">ID Card</label>
+                            <input type="text" name="uuid" id="idcard" class="text-center form-control col-6" placeholder="Harap Scan ID Card" readonly value="coba">
                         </div>
-                        <div class="form-group text-center">
-                            <button type="submit" name="tambah-kelas" class="btn btn-success">Simpan</button>
+                        <div class="form-group d-flex">
+                            <label for="kelas" class="col-4 mt-2 text-center">Kelas</label>
+                            <select class="custom-select col-6 text-center" id="kelas" required>
+                                <option value="" disabled selected>Select an option</option>
+                                <?php foreach (seluruhKelas($db) as $kelas) : ?>
+                                    <option value="<?= $kelas['id_kelas'] ?>"><?= $kelas['nama_kelas'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+
+
+                        <div class="form-group text-center mt-5">
+                            <button type="submit" name="tambah-siswa" class="btn btn-success">Simpan</button>
+                            <a href="siswa" class="btn btn-danger ml-3">Batal</a>
                         </div>
                     </form>
                 </div>
