a17396fe518003730c86d73c2a78a0eee9117e32
Wahyu Nur Cahyo <116796627+whyyy24@users.noreply.github.com>
2023-07-29 17:05:34 +0700

    .

--- public/view/layouts/admin/tambah_guru.php
+++ public/view/layouts/admin/tambah_guru.php
@@ -5,37 +5,29 @@ include "public/view/partials/admin/admin_sidebar.php";
 include "public/view/partials/admin/admin_wrapper.php";
 include "public/view/partials/admin/admin_modal.php";
 ?>
-<?php
 
-$id_kariyawan = decrypt(htmlspecialchars($_GET['id']), $key);
-if (isset($_POST['edit-kariyawan'])) {
-    $namaK = htmlspecialchars($_POST['nama-kariyawan']);
-    $jabatan = htmlspecialchars($_POST['jabatan']);
-    $mulai = htmlspecialchars($_POST['mulai']);
-    $jenis = htmlspecialchars($_POST['jenis']);
-    $id_akun = htmlspecialchars($_GET['ak']);
-    $username = htmlspecialchars($_POST['username']);
-    $password = htmlspecialchars($_POST['password-akun']);
-    mysqli_query($db, "UPDATE `kariyawan` SET `nama_kariyawan` = '$namaK', `jenis` = '$jabatan', `mulai` = '$mulai',  WHERE `kariyawan`.`id_kariyawan` = '$id_kariyawan'");
-    if (!$password) {
-        mysqli_query($db, "UPDATE akun SET username = '$username' WHERE id_akun = '$id_akun'");
-        echo "<h1>Lorem, ipsum dolor.</h1>";
-    } else {
-        mysqli_query($db, "UPDATE akun SET username = '$username', password = MD5('$password') WHERE id_akun = '$id_akun'");
-    }
-}
-
-$getdata = mysqli_query($db, "SELECT * FROM kariyawan LEFT JOIN akun ON akun.id_akun = kariyawan.id_akun WHERE kariyawan.id_kariyawan = '$id_kariyawan'");
-$dataKariyawan = mysqli_fetch_array($getdata);
-?>
 <!-- Begin Page Content -->
 <div class="container-fluid">
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Edit <?= $dataKariyawan['nama_kariyawan'] ?></h1>
+        <h1 class="h3 mb-0 text-gray-800">Tambah Data Kariyawan</h1>
     </div>
     <?php
     include "query/admin/kelas.php";
+    ?>
+    <?php
+    if (isset($_POST['tambah-kariyawan'])) {
+        $id_akun = date('Ymdhis');
+        $namaK = htmlspecialchars($_POST['nama-kariyawan']);
+        $jabatan = htmlspecialchars($_POST['jabatan']);
+        $mulai = htmlspecialchars($_POST['mulai']);
+        $jenis_akun = htmlspecialchars($_POST['jenis']);
+        $username = htmlspecialchars($_POST['username']);
+        $password = htmlspecialchars($_POST['password-akun']);
+        $akun = mysqli_query($db, "INSERT INTO `akun` (`id_akun`, `username`, `password`, `level_akun`) VALUES ('$id_akun', '$username', '$password', '$jenis_akun')");
+        $update = mysqli_query($db, "INSERT INTO `kariyawan` (`id_kariyawan`, `nama_kariyawan`, `jenis`, `mulai`, `id_akun`, `id_kelas`) VALUES (NULL, '$namaK', '$jabatan', '$mulai', '$id_akun', NULL)");
+    }
+
     ?>
     <?php
     if (isset($stat) == "berhasil") {
@@ -59,9 +51,9 @@ $dataKariyawan = mysqli_fetch_array($getdata);
             <div class="card shadow mb-4">
                 <div class="p-3">
                     <div class="text-center">
-                        <h4 class="text-gray-900 m-4">Form Edit Guru/Kariyawan</h4>
+                        <h4 class="text-gray-900 m-4">Form Tambah Guru/Kariyawan</h4>
                     </div>
-                    <form action="?hlm=edit-guru&id=<?= $_GET['id'] ?>" method="post" class="user">
+                    <form action="?hlm=tambah-guru" method="post" class="user">
                         <div class="form-group d-flex">
                             <label for="nama-kariyawan" class="col-4 mt-2 text-center">Nama Kariyawan</label>
                             <input type="text" name="nama-kariyawan" id="nama-kariyawan" class="text-center form-control col-6" placeholder="Nama Kariyawan">
